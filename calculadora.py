69ff007d21725b399e6ceae468da64b0d4dacbab
Gabriel Leonangeli <leonangeli.gl@gmail.com>
2025-10-03 19:56:29 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,33 @@
+print ("Bienvenidos a la calculadora")
+print ("Para salir escribe salir")
+print ("Las operaciones son suma , resta, multi y div")
+
+
+resultado = ""
+while True:
+    if not resultado:
+        resultado = input ("ingrese numero: ")
+        if resultado.lower() == "salir":
+            break
+        resultado = int(resultado)
+    op = input ("ingrese operacion: ")
+    if op.lower() == "salir":
+        break
+    n2 = input ("ingrese siguiente numero: ")
+    if op.lower() == "salir":
+        break
+    n2 = int(n2)
+    if op.lower() == "suma":
+        resultado += n2
+    elif op.lower() == "resta":
+        resultado -= n2
+    elif op.lower() == "multi":
+        resultado *= n2
+    elif op.lower() == "div":
+        resultado /= n2
+    else:
+      print ("operacion no valida")
+      break
+
+    print (f"El resultado es {resultado}")
+    
